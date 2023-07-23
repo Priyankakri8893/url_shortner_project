53be084bab54f897fdbc5d427094896db6d64285
Priyanka <priyankakri8893@gmail.com>
2023-07-23 20:17:27 +0530

    redis part comments

--- src/aws/redis.js
+++ src/aws/redis.js
@@ -1,30 +1,30 @@
-const redis = require('redis');
+// const redis = require('redis');
 
-// Load environment variables from .env file
-require("dotenv").config();
+// // Load environment variables from .env file
+// require("dotenv").config();
 
-const { promisify } = require('util');
+// const { promisify } = require('util');
 
-// Connect to the Redis server using the environment variables
-const Client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
-  no_ready_check: true
-});
+// // Connect to the Redis server using the environment variables
+// const Client = redis.createClient({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT,
+//   password: process.env.REDIS_PASSWORD,
+//   no_ready_check: true
+// });
 
-// Authentication callback
-Client.auth(process.env.REDIS_PASSWORD, function (err) {
-  if (err) throw err;
-});
+// // Authentication callback
+// Client.auth(process.env.REDIS_PASSWORD, function (err) {
+//   if (err) throw err;
+// });
 
-// Event listener for successful connection
-Client.on('connect', async function () {
-  console.log('Connected to Redis...');
-});
+// // Event listener for successful connection
+// Client.on('connect', async function () {
+//   console.log('Connected to Redis...');
+// });
 
-// Promisify Redis commands
-const SETEX_ASYNC = promisify(Client.SETEX).bind(Client);
-const GET_ASYNC = promisify(Client.GET).bind(Client);
+// // Promisify Redis commands
+// const SETEX_ASYNC = promisify(Client.SETEX).bind(Client);
+// const GET_ASYNC = promisify(Client.GET).bind(Client);
 
-module.exports= {SETEX_ASYNC, GET_ASYNC}
+// module.exports= {SETEX_ASYNC, GET_ASYNC}
